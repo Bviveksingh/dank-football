eb1a952d8839a44d6b7c64b2af2b53022019a1a7
Vivek Bisht <bviveksingh96@gmail.com>
2020-01-26 09:42:08 +0530

    Few more changes made

--- src/app/api-calls/feature-image.service.ts
+++ src/app/api-calls/feature-image.service.ts
@@ -10,21 +10,10 @@ export class FeatureImageService {
   private imgurUrl: string = 'https://api.imgur.com/3/image';
  
   private imageLink: string;
-  private token:any = '0a64656d315e3cce26277133f36b33f9c8e97a5b';
-  private clientId: any = '1d97dcaf9d7b104';
+  
+  private clientId: any = 'YOUR_CLIENT_ID';
   constructor(private http:HttpClient) {}
 
-  // uploadImage(imageFile:File): Observable<any>{  
-  //   let formData = new FormData();
-  //   formData.append('image',imageFile, imageFile.name);
-
-
-  //   let headers = new HttpHeaders({
-  //     "authorization": 'Client-ID '+this.clientId
-  //   })
-  //   return this.http.post(this.imgurUrl , formData, {headers:headers});
-
-  // }
   uploadImage(imageFile:File){  
     let formData = new FormData();
     formData.append('image',imageFile, imageFile.name);
@@ -37,9 +26,7 @@ export class FeatureImageService {
     return this.http.post(this.imgurUrl , formData, {headers:headers});
 
     
-    // this.imageLink = imageData['data'].link;
-
-    // return this.imageLink;    
+    
 
   }
 }
